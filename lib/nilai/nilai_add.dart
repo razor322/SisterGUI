c3aa38330849a3dc88c4c7cbe14e403c161a86d0
gybran nauval <48706438+razor322@users.noreply.github.com>
2023-12-28 22:50:43 +0700

    Update nilai_add.dart

--- lib/nilai/nilai_add.dart
+++ lib/nilai/nilai_add.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 //gybran nauval yuhandika 2111081006
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
